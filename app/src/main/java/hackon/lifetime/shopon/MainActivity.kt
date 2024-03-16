4be9d47030f856d5e8ec2b7befc86b09d806a9be
prasad <prasadbhujbal1407@gmail.com>
2024-03-16 07:10:48 +0530

    Added Products

--- app/src/main/java/hackon/lifetime/shopon/MainActivity.kt
+++ app/src/main/java/hackon/lifetime/shopon/MainActivity.kt
@@ -8,21 +8,21 @@ import hackon.lifetime.shopon.models.Product
 
 val products = mutableListOf(
     Product(
-        1, "Wooden Chair", "4,900", R.drawable.wooden_chair,
+        1, "Wooden Chair", "3,999", R.drawable.wooden_chair,
         "100+ Bought in the last month",
         "Acacia Wood and Natural Cane Weaving",
         4.5f, "1,210",
         "https://firebasestorage.googleapis.com/v0/b/whatschat-1348c.appspot.com/o/chair.glb?alt=media&token=e32cf525-ee71-4490-a38e-0c42f49be3b2"
     ),
     Product(
-        2, "Canteen Table", "4,500", R.drawable.table,
+        2, "Canteen Table", "3,500", R.drawable.table,
         "1k+ Bought in the last month",
         "Crafted of Oak wood legs, the sleek veneer top showcases a rick woodgrain finish.",
         4f, "895",
         "https://firebasestorage.googleapis.com/v0/b/whatschat-1348c.appspot.com/o/canteenTable.glb?alt=media&token=c6d913d9-847c-4e4c-8907-51ccf0b56c96"
     ),
     Product(
-        3, "Pedestal Fan", "3,200", R.drawable.pedestal_fan,
+        3, "Pedestal Fan", "1,200", R.drawable.pedestal_fan,
         "200 Bought in the last month",
         "Pedestal Fan Wind Storm 18 inch features a powerful energy efficient heavy duty motor, telescopic height adjustment and three-speed control.",
         3.5f, "4,752",
@@ -36,7 +36,7 @@ val products = mutableListOf(
         "https://firebasestorage.googleapis.com/v0/b/whatschat-1348c.appspot.com/o/Tripod%20Grip.glb?alt=media&token=c8c8650f-8980-4848-ba46-130b7d90cc83"
     ),
     Product(
-        5, "Office Chair", "11,880", R.drawable.office_chair,
+        5, "Office Chair", "7,880", R.drawable.office_chair,
         "215 Bought in the last month",
         "Chair with lumbar support and pneumatic gas lift for height adjustment and 360 degree swivel.",
         4.5f, "257",
@@ -50,14 +50,14 @@ val products = mutableListOf(
         "https://firebasestorage.googleapis.com/v0/b/aadhar-address-updation.appspot.com/o/bar_chair.glb?alt=media&token=f17deee7-1bff-4ff0-86ea-9542f68146cb"
     ),
     Product(
-        7, "Park Bench", "12,500", R.drawable.park_bench_photo,
+        7, "Park Bench", "9,500", R.drawable.park_bench_photo,
         "100+ Bought in the last month",
         "The large garden bench seat pad offers the perfect mix of appearance and functionality",
         3.5f, "578",
         "https://firebasestorage.googleapis.com/v0/b/aadhar-address-updation.appspot.com/o/park_bench.glb?alt=media&token=8afb7436-7dc2-411e-9b9a-3583c076fc8f"
     ),
     Product(
-        8, "Microwave Oven", "15,000", R.drawable.oven_photo,
+        8, "Microwave Oven", "8,000", R.drawable.oven_photo,
         "1k+ Bought in the last month",
         "GRILL, BAKE & TOAST - Use the oven for baking cakes, pizzas and pastas, grilling vegetables, roasting potatoes, chicken, paneer or simply toasting bread.",
         4.5f, "734",
